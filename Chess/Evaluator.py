f5ed296a657c8cfaeedf266561d55bcdcfd11564
Olga R <80749258+TheOlgen@users.noreply.github.com>
2025-04-25 20:08:04 +0200

    add Evaluator

---
+++ Chess/Evaluator.py
@@ -0,0 +1,12 @@
+from stockfish import Stockfish
+
+class Evaluator:
+    def __init__(self):
+        self.sf = Stockfish(
+            path="Stockfish/stockfish.exe",
+            parameters={"Threads": 8, "Hash": 1024}
+        )
+
+    def evaluate(self, fen_position):
+        self.sf.set_fen_position(fen_position)
+        return self.sf.get_best_move(), self.sf.get_evaluation()
--- Chess/StockfistTest.py
+++ Chess/StockfistTest.py
@@ -1,12 +1,8 @@
 
-from stockfish import Stockfish
+import Evaluator
 
-sf = Stockfish(
-        path="Stockfish/stockfish.exe",
-        parameters={"Threads": 8, "Hash": 1024}
-     )
-sf.set_fen_position("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
-sf._go()
 
-print(sf.get_best_move())         # ruch w UCI, np. e2e4
-print(sf.get_evaluation())        # {'type': 'cp', 'value': 23}
+E = Evaluator.Evaluator()
+
+print(E.evaluate("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"))
+
