85fb58f32545da9eefa6ce43ee6e264c9200d8d7
Olga R <80749258+TheOlgen@users.noreply.github.com>
2025-05-26 09:32:31 +0200

    new db records

--- Chess/Generators/BestMoveGenerator.py
+++ Chess/Generators/BestMoveGenerator.py
@@ -5,17 +5,20 @@ from chess.Database.SQL_chess import init_db, add_position, show_database
 # Inicjalizacja
 evaluator = eval.Evaluator()
 file = open("bestMoveData.txt", 'a')
-init_db()
+#init_db()
 
 with open('chessData.csv', newline='', encoding='utf-8') as csvfile:
     reader = csv.reader(csvfile)
     next(reader, None)
+    count = 0
     for row in reader:
+        count += 1
         fen = row[0]
         best_move = evaluator.evaluate(fen)
         #file.write(fen + " ; " + best_move + "\n") #debug
         add_position(fen, best_move)
         #to zakomentujcie jeśli chcecie coś dodawać do bazy
+        #if count == 50:
         break
 
 
